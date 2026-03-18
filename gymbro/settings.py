72466923d87fd1092c2f9f136942dcbc705e0892
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-18 19:14:14 +0100

    feat: add crispy-forms and crispy-bootstrap5 to settings.py

--- gymbro/settings.py
+++ gymbro/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #Third party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    #Local
     "tracker"
 ]
 
@@ -117,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
