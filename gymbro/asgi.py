200c4e3395046d41de2fc23ab9a8b8b55998a80f
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-18 18:50:39 +0100

    feat: rename main directory

--- gymbro/asgi.py
+++ gymbro/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for GymBro project.
+ASGI config for gymbro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GymBro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymbro.settings")
 
 application = get_asgi_application()
--- gymbro/wsgi.py
+++ gymbro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for GymBro project.
+WSGI config for gymbro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GymBro.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gymbro.settings")
 
 application = get_wsgi_application()
