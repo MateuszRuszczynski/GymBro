f1cc83174e750f066197bdab7c7afeb1f8dc533f
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-19 16:28:32 +0100

    feat: implement ExerciseListView

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from tracker.models import Exercise
+
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "tracker/index.html")
+
+
+class ExerciseListView(ListView):
+    model = Exercise
+
