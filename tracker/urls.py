ed80186d38763e1bb6485743e695715007117faa
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-19 17:12:46 +0100

    feat: add path urls for list view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path
 
-from tracker import views
+from .views import (
+    index,
+    ExerciseListView,
+    MuscleGroupListView,
+    GymUserListView,
+    WorkoutLogListView,
+    WorkoutPlanListView
+)
+
+app_name = "tracker"
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", index, name="index"),
+    path("exercises/", ExerciseListView.as_view(), name="exercise-list"),
+    path("musclegroups/", MuscleGroupListView.as_view(), name="muscle-group-list"),
+    path("gymusers/", GymUserListView.as_view(), name="gymu-uer-list"),
+    path("workoutlogs/", WorkoutLogListView.as_view(), name="workout-log-list"),
+    path("workoutplans/", WorkoutPlanListView.as_view(), name="workout-plan-list"),
 ]
