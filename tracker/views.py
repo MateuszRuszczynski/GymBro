879afc141f91983e162daec2617362e34a19afe2
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-19 17:14:27 +0100

    feat: implement class ListView for each model

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from tracker.models import Exercise
+from tracker.models import Exercise, MuscleGroup, GymUser, WorkoutLog, WorkoutPlan
 
 
 def index(request):
@@ -11,3 +11,19 @@ def index(request):
 class ExerciseListView(ListView):
     model = Exercise
 
+
+class MuscleGroupListView(ListView):
+    model = MuscleGroup
+
+
+class GymUserListView(ListView):
+    model = GymUser
+    ordering = ["username"]
+
+
+class WorkoutLogListView(ListView):
+    model = WorkoutLog
+
+
+class WorkoutPlanListView(ListView):
+    model = WorkoutPlan
