05b105929c052299e790fdc419e42e7a5797e392
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-20 17:32:17 +0100

    feat: implement class detail view for each model

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from tracker.models import Exercise, MuscleGroup, GymUser, WorkoutLog, WorkoutPlan
 
@@ -13,9 +13,19 @@ class ExerciseListView(ListView):
     context_object_name = "exercises"
 
 
+class ExerciseDetailView(DetailView):
+    model = Exercise
+    context_object_name = "exercise"
+
+
 class MuscleGroupListView(ListView):
     model = MuscleGroup
     context_object_name = "muscle_groups"
+
+
+class MuscleGroupDetailView(DetailView):
+    model = MuscleGroup
+    context_object_name = "muscle_group"
 
 
 class GymUserListView(ListView):
@@ -24,11 +34,26 @@ class GymUserListView(ListView):
     ordering = ["username"]
 
 
+class GymUserDetailView(DetailView):
+    model = GymUser
+    context_object_name = "gym_user"
+
+
 class WorkoutLogListView(ListView):
     model = WorkoutLog
     context_object_name = "workout_logs"
 
 
+class WorkoutLogDetailView(DetailView):
+    model = WorkoutLog
+    context_object_name = "workout_log"
+
+
 class WorkoutPlanListView(ListView):
     model = WorkoutPlan
     context_object_name = "workout_plans"
+
+
+class WorkoutPlanDetailView(DetailView):
+    model = WorkoutPlan
+    context_object_name = "workout_plan"
