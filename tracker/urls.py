235dd6c5b6305bb5770480a18d6966630089857d
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-20 17:34:06 +0100

    feat: add urls for detail view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,21 +1,19 @@
 from django.urls import path
+from tracker import views
 
-from .views import (
-    index,
-    ExerciseListView,
-    MuscleGroupListView,
-    GymUserListView,
-    WorkoutLogListView,
-    WorkoutPlanListView
-)
 
 app_name = "tracker"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("exercises/", ExerciseListView.as_view(), name="exercise-list"),
-    path("musclegroups/", MuscleGroupListView.as_view(), name="muscle-group-list"),
-    path("gymusers/", GymUserListView.as_view(), name="gym-user-list"),
-    path("workoutlogs/", WorkoutLogListView.as_view(), name="workout-log-list"),
-    path("workoutplans/", WorkoutPlanListView.as_view(), name="workout-plan-list"),
+    path("", views.index, name="index"),
+    path("exercises/", views.ExerciseListView.as_view(), name="exercise-list"),
+    path("exercises/<int:pk>/", views.ExerciseDetailView.as_view(), name="exercise-detail"),
+    path("musclegroups/", views.MuscleGroupListView.as_view(), name="muscle-group-list"),
+    path("musclegroups/<int:pk>/", views.MuscleGroupDetailView.as_view(), name="muscle-group-detail"),
+    path("gymusers/", views.GymUserListView.as_view(), name="gym-user-list"),
+    path("gymusers/<int:pk>/", views.GymUserDetailView.as_view(), name="gym-user-detail"),
+    path("workoutlogs/", views.WorkoutLogListView.as_view(), name="workout-log-list"),
+    path("workoutlogs/<int:pk>/", views.WorkoutLogDetailView.as_view(), name="workout-log-detail"),
+    path("workoutplans/", views.WorkoutPlanListView.as_view(), name="workout-plan-list"),
+    path("workoutplans/<int:pk>/", views.WorkoutPlanDetailView.as_view(), name="workout-plan-detail"),
 ]
