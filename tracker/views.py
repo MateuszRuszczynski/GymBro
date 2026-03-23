ae248340c969bdd308e4c764834dbba6f63b1d18
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-23 14:21:42 +0100

    feat: create CBV for create view, add queryset attribute

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
 
 from tracker.models import Exercise, MuscleGroup, GymUser, WorkoutLog, WorkoutPlan
 
@@ -11,11 +12,17 @@ def index(request):
 class ExerciseListView(ListView):
     model = Exercise
     context_object_name = "exercises"
+    queryset = Exercise.objects.select_related("muscle_group")
 
 
 class ExerciseDetailView(DetailView):
     model = Exercise
     context_object_name = "exercise"
+
+
+class ExerciseCreateView(CreateView):
+    model = Exercise
+    success_url = reverse_lazy("tracker:exercise-list")
 
 
 class MuscleGroupListView(ListView):
@@ -26,6 +33,11 @@ class MuscleGroupListView(ListView):
 class MuscleGroupDetailView(DetailView):
     model = MuscleGroup
     context_object_name = "muscle_group"
+
+
+class MuscleGroupCreateView(CreateView):
+    model = MuscleGroup
+    success_url = reverse_lazy("tracker:muscle-group-list")
 
 
 class GymUserListView(ListView):
@@ -39,9 +51,18 @@ class GymUserDetailView(DetailView):
     context_object_name = "gym_user"
 
 
+class GymUserCreateView(CreateView):
+    model = GymUser
+    success_url = reverse_lazy("tracker:gym-user-list")
+
+
 class WorkoutLogListView(ListView):
     model = WorkoutLog
     context_object_name = "workout_logs"
+    queryset = WorkoutLog.objects.select_related(
+        "user",
+        "workout_plan",
+    )
 
 
 class WorkoutLogDetailView(DetailView):
@@ -49,11 +70,22 @@ class WorkoutLogDetailView(DetailView):
     context_object_name = "workout_log"
 
 
+class WorkoutLogCreateView(CreateView):
+    model = WorkoutLog
+    success_url = reverse_lazy("tracker:workout-log-list")
+
+
 class WorkoutPlanListView(ListView):
     model = WorkoutPlan
     context_object_name = "workout_plans"
+    queryset = WorkoutPlan.objects.select_related("created_by").prefetch_related("exercises")
 
 
 class WorkoutPlanDetailView(DetailView):
     model = WorkoutPlan
     context_object_name = "workout_plan"
+
+
+class WorkoutPlanCreateView(CreateView):
+    model = WorkoutPlan
+    success_url = reverse_lazy("tracker:workout-plan-list")
