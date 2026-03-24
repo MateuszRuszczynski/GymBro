405ae9eb979ced521be74ea891c11601594f0ef7
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-24 14:25:43 +0100

    feat: change muscle group model

--- tracker/models.py
+++ tracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class MuscleGroup(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
