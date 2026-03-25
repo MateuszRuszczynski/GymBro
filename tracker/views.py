c06a99476ba0748fac55e51d65c65dec2c8189f9
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-25 14:15:47 +0100

    feat: add @login_required and LoginRequiredMixin in the views.py

--- tracker/views.py
+++ tracker/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
@@ -9,6 +11,7 @@ from tracker.forms import ExerciseForm, WorkoutPlanForm, GymUserCreationForm, Gy
 from tracker.models import Exercise, MuscleGroup, GymUser, WorkoutLog, WorkoutPlan
 
 
+@login_required
 def index(request):
     context = {
         "total_exercises": Exercise.objects.count(),
@@ -25,6 +28,7 @@ def index(request):
     }
     return render(request, "tracker/index.html", context=context)
 
+
 def register(request):
     if request.method == "POST":
         form = GymUserCreationForm(request.POST)
@@ -36,7 +40,8 @@ def register(request):
         form = GymUserUpdateForm()
     return render(request, "registration/register.html", {"form": form})
 
-class ExerciseListView(ListView):
+
+class ExerciseListView(LoginRequiredMixin, ListView):
     model = Exercise
     context_object_name = "exercises"
 
@@ -51,94 +56,94 @@ class ExerciseListView(ListView):
         return queryset
 
 
-class ExerciseDetailView(DetailView):
+class ExerciseDetailView(LoginRequiredMixin, DetailView):
     model = Exercise
     context_object_name = "exercise"
     queryset = Exercise.objects.select_related("muscle_group")
 
 
-class ExerciseCreateView(CreateView):
+class ExerciseCreateView(LoginRequiredMixin, CreateView):
     model = Exercise
     context_object_name = "exercise"
     form_class = ExerciseForm
     success_url = reverse_lazy("tracker:exercise-list")
 
 
-class ExerciseUpdateView(UpdateView):
+class ExerciseUpdateView(LoginRequiredMixin, UpdateView):
     model = Exercise
     context_object_name = "exercise"
     form_class = ExerciseForm
     success_url = reverse_lazy("tracker:exercise-list")
 
 
-class ExerciseDeleteView(DeleteView):
+class ExerciseDeleteView(LoginRequiredMixin, DeleteView):
     model = Exercise
     context_object_name = "exercise"
     success_url = reverse_lazy("tracker:exercise-list")
 
 
-class MuscleGroupListView(ListView):
+class MuscleGroupListView(LoginRequiredMixin, ListView):
     model = MuscleGroup
     context_object_name = "muscle_groups"
 
 
-class MuscleGroupDetailView(DetailView):
+class MuscleGroupDetailView(LoginRequiredMixin, DetailView):
     model = MuscleGroup
     context_object_name = "muscle_group"
 
 
-class MuscleGroupCreateView(CreateView):
-    model = MuscleGroup
-    form_class = MuscleGroupForm
-    context_object_name = "muscle_group"
-    success_url = reverse_lazy("tracker:muscle-group-list")
-
-
-class MuscleGroupUpdateView(UpdateView):
+class MuscleGroupCreateView(LoginRequiredMixin, CreateView):
     model = MuscleGroup
     form_class = MuscleGroupForm
     context_object_name = "muscle_group"
     success_url = reverse_lazy("tracker:muscle-group-list")
 
 
-class MuscleGroupDeleteView(DeleteView):
+class MuscleGroupUpdateView(LoginRequiredMixin, UpdateView):
+    model = MuscleGroup
+    form_class = MuscleGroupForm
+    context_object_name = "muscle_group"
+    success_url = reverse_lazy("tracker:muscle-group-list")
+
+
+class MuscleGroupDeleteView(LoginRequiredMixin, DeleteView):
     model = MuscleGroup
     context_object_name = "muscle_group"
     success_url = reverse_lazy("tracker:muscle-group-list")
 
 
-class GymUserListView(ListView):
+class GymUserListView(LoginRequiredMixin, ListView):
     model = GymUser
     context_object_name = "gym_users"
     ordering = ["username"]
 
 
-class GymUserDetailView(DetailView):
+class GymUserDetailView(LoginRequiredMixin, DetailView):
     model = GymUser
     context_object_name = "gym_user"
 
 
-class GymUserCreateView(CreateView):
+class GymUserCreateView(LoginRequiredMixin, CreateView):
     model = GymUser
     context_object_name = "gym_user"
     form_class = GymUserCreationForm
     success_url = reverse_lazy("tracker:gym-user-list")
 
 
-class GymUserUpdateView(UpdateView):
+class GymUserUpdateView(LoginRequiredMixin, UpdateView):
     model = GymUser
     context_object_name = "gym_user"
     form_class = GymUserUpdateForm
     success_url = reverse_lazy("tracker:gym-user-list")
 
 
-class GymUserDeleteView(DeleteView):
+class GymUserDeleteView(LoginRequiredMixin, DeleteView):
     model = GymUser
     context_object_name = "gym_user"
     success_url = reverse_lazy("tracker:gym-user-list")
 
 
-class WorkoutLogListView(ListView):
+class WorkoutLogListView(LoginRequiredMixin, ListView):
     model = WorkoutLog
     context_object_name = "workout_logs"
     queryset = WorkoutLog.objects.select_related(
@@ -147,7 +152,7 @@ class WorkoutLogListView(ListView):
     )
 
 
-class WorkoutLogDetailView(DetailView):
+class WorkoutLogDetailView(LoginRequiredMixin, DetailView):
     model = WorkoutLog
     context_object_name = "workout_log"
     queryset = WorkoutLog.objects.select_related(
@@ -156,27 +161,27 @@ class WorkoutLogDetailView(DetailView):
     )
 
 
-class WorkoutLogCreateView(CreateView):
+class WorkoutLogCreateView(LoginRequiredMixin, CreateView):
     model = WorkoutLog
     context_object_name = "workout_log"
     form_class = WorkoutLogForm
     success_url = reverse_lazy("tracker:workout-log-list")
 
 
-class WorkoutLogUpdateView(UpdateView):
+class WorkoutLogUpdateView(LoginRequiredMixin, UpdateView):
     model = WorkoutLog
     context_object_name = "workout_log"
     form_class = WorkoutLogForm
     success_url = reverse_lazy("tracker:workout-log-list")
 
 
-class WorkoutLogDeleteView(DeleteView):
+class WorkoutLogDeleteView(LoginRequiredMixin, DeleteView):
     model = WorkoutLog
     context_object_name = "workout_log"
     success_url = reverse_lazy("tracker:workout-log-list")
 
 
-class WorkoutPlanListView(ListView):
+class WorkoutPlanListView(LoginRequiredMixin, ListView):
     model = WorkoutPlan
     context_object_name = "workout_plans"
     queryset = WorkoutPlan.objects.select_related(
@@ -184,7 +189,7 @@ class WorkoutPlanListView(ListView):
     ).prefetch_related("exercises")
 
 
-class WorkoutPlanDetailView(DetailView):
+class WorkoutPlanDetailView(LoginRequiredMixin, DetailView):
     model = WorkoutPlan
     context_object_name = "workout_plan"
     queryset = WorkoutPlan.objects.select_related(
@@ -192,21 +197,21 @@ class WorkoutPlanDetailView(DetailView):
     ).prefetch_related("exercises")
 
 
-class WorkoutPlanCreateView(CreateView):
+class WorkoutPlanCreateView(LoginRequiredMixin, CreateView):
     model = WorkoutPlan
     context_object_name = "workout_plan"
     form_class = WorkoutPlanForm
     success_url = reverse_lazy("tracker:workout-plan-list")
 
 
-class WorkoutPlanUpdateView(UpdateView):
+class WorkoutPlanUpdateView(LoginRequiredMixin, UpdateView):
     model = WorkoutPlan
     context_object_name = "workout_plan"
     form_class = WorkoutPlanForm
     success_url = reverse_lazy("tracker:workout-plan-list")
 
 
-class WorkoutPlanDeleteView(DeleteView):
+class WorkoutPlanDeleteView(LoginRequiredMixin, DeleteView):
     model = WorkoutPlan
     context_object_name = "workout_plan"
     success_url = reverse_lazy("tracker:workout-plan-list")
