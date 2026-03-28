10ae8e57f80a302e7cbc3ac5490dddb308fb647e
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-28 13:50:47 +0100

    feat: create tests: str, muscle group relationship, difficulty choices for Exercise model

--- tracker/tests/test_models.py
+++ tracker/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from tracker.models import MuscleGroup, GymUser
+from tracker.models import MuscleGroup, GymUser, Exercise
 
 
 class MuscleGroupTests(TestCase):
@@ -35,3 +35,24 @@ class GymUserTests(TestCase):
         valid_choices = ["free", "premium", "coach"]
         self.assertIn(self.gym_user.membership_type, valid_choices)
 
+
+class ExerciseTests(TestCase):
+    def setUp(self):
+        self.muscle_group = MuscleGroup.objects.create(name="Biceps")
+        self.exercise = Exercise.objects.create(
+            name="Leg press",
+            description="One of the best exercise for legs",
+            difficulty="intermediate",
+            equipment="barbell",
+            muscle_group=self.muscle_group,
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.exercise.name), "Leg press")
+
+    def test_muscle_group_relationship(self):
+        self.assertEqual(str(self.muscle_group.name), "Biceps")
+
+    def test_difficulty_choices(self):
+        valid_choices = ["beginner", "intermediate", "advanced"]
+        self.assertIn(self.exercise.difficulty, valid_choices)
