dba0a3c05793f68b4e6a1d7ce4d5debb57b0ff46
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-28 14:06:52 +0100

    feat: create test: str, is personal record, user relationship

--- tracker/tests/test_models.py
+++ tracker/tests/test_models.py
@@ -1,6 +1,8 @@
+from datetime import date
+
 from django.test import TestCase
 
-from tracker.models import MuscleGroup, GymUser, Exercise, WorkoutPlan
+from tracker.models import MuscleGroup, GymUser, Exercise, WorkoutPlan, WorkoutLog
 
 
 class MuscleGroupTests(TestCase):
@@ -58,7 +60,7 @@ class ExerciseTests(TestCase):
         self.assertIn(self.exercise.difficulty, valid_choices)
 
 
-class WorkoutPlanModelTest(TestCase):
+class WorkoutPlanTest(TestCase):
     def setUp(self):
         self.user = GymUser.objects.create_user(
             username="Willy",
@@ -86,3 +88,76 @@ class WorkoutPlanModelTest(TestCase):
 
     def test_created_by_relationship(self):
         self.assertEqual(self.plan.created_by.username, "Willy")
+
+
+class WorkoutPlanModelTest(TestCase):
+    def setUp(self):
+        self.user = GymUser.objects.create_user(
+            username="testuser",
+            password="testpass123",
+        )
+        self.muscle_group = MuscleGroup.objects.create(name="Legs")
+        self.exercise = Exercise.objects.create(
+            name="Squat",
+            difficulty="intermediate",
+            equipment="Barbell",
+            muscle_group=self.muscle_group,
+        )
+        self.plan = WorkoutPlan.objects.create(
+            name="Leg Day",
+            goal="strength",
+            created_by=self.user,
+        )
+        self.plan.exercises.add(self.exercise)
+
+    def test_str(self):
+        self.assertEqual(str(self.plan), "Leg Day")
+
+    def test_exercises_relationship(self):
+        self.assertIn(self.exercise, self.plan.exercises.all())
+
+    def test_created_by_relationship(self):
+        self.assertEqual(self.plan.created_by.username, "testuser")
+
+
+class WorkoutLogTest(TestCase):
+    def setUp(self):
+        self.user = GymUser.objects.create_user(
+            username="Andrew",
+            password="testpass123",
+        )
+        self.muscle_group = MuscleGroup.objects.create(name="Back")
+        self.exercise = Exercise.objects.create(
+            name="Deadlift",
+            difficulty="advanced",
+            equipment="Barbell",
+            muscle_group=self.muscle_group,
+        )
+        self.plan = WorkoutPlan.objects.create(
+            name="Pull Day",
+            goal="strength",
+            created_by=self.user,
+        )
+        self.log = WorkoutLog.objects.create(
+            user=self.user,
+            workout_plan=self.plan,
+            date=date.today(),
+            duration_minutes=60,
+            notes="Great session!",
+            is_personal_record=True,
+        )
+
+    def test_str(self):
+        self.assertEqual(str(self.log), "Pull Day")
+
+    def test_is_personal_record_default_false(self):
+        log = WorkoutLog.objects.create(
+            user=self.user,
+            workout_plan=self.plan,
+            date=date.today(),
+            duration_minutes=45,
+        )
+        self.assertFalse(log.is_personal_record)
+
+    def test_user_relationship(self):
+        self.assertEqual(self.log.user.username, "Andrew")
