a23556d981521a59972d8929bc49d2ca9942a424
Mateusz <mateusz.ruszczynski@gmail.com>
2026-03-28 13:39:44 +0100

    feat: create tests: str, default membetship type, membership choices for GymUser model

--- tracker/tests/test_model.py
+++ tracker/tests/test_model.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from tracker.models import MuscleGroup
+from tracker.models import MuscleGroup, GymUser
 
 
 class MuscleGroupTests(TestCase):
@@ -11,5 +11,27 @@ class MuscleGroupTests(TestCase):
         self.assertEqual(str(self.muscle_group), "Legs")
 
 
+class GymUserTests(TestCase):
+    def setUp(self):
+        self.gym_user = GymUser.objects.create_user(
+            username="Marco_body_builder",
+            password="test123",
+            first_name="Marco",
+            last_name="Doe",
+            membership_type="premium"
+        )
 
+    def test_str(self):
+        self.assertEqual(str(self.gym_user), "Marco Doe")
+
+    def test_default_membership_type(self):
+        user = GymUser.objects.create_user(
+            username="Joe",
+            password="testpass123",
+        )
+        self.assertEqual(user.membership_type, "free")
+
+    def test_membership_choices(self):
+        valid_choices = ["free", "premium", "coach"]
+        self.assertIn(self.gym_user.membership_type, valid_choices)
 
